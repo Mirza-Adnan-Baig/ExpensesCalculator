f16d73d6b2557345c7a1bca298af8be7d40607f7
Mirza Adnan Baig <mirza.adnan.baig01@gmail.com>
2022-12-25 18:42:22 +0100

    first commit

---
+++ src/Components/Expenses/ExpenseItem.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import Card from "../UI/Card";
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  // function clickHandler
+  // const [title, setTitle] = useState(props.title);
+  // const clickHandler = (props) => {
+  //   setTitle("updated!");
+  //   console.log(title);
+  return (
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">€{props.amount}</div>
+        </div>
+      </Card>
+    </li>
+  );
+};
+
+export default ExpenseItem;
